d0445fefa6e2642ea20bdb494b4f614c0879b77f
shahzadakram786 <alyakram786@gmail.com>
2024-12-05 23:48:03 +0500

    working on the crud

---
+++ crud/data.js
@@ -0,0 +1,36 @@
+
+const users = [
+    { id: 1, name: "shahzad akram" },
+    { id: 2, name: "ajaml islam" },
+    { id: 3, name: "abbas khan" },
+  ];
+  
+  // console.log(users)
+
+// users.push({id:4 , name: 'aliyan'})
+// console.log(users)
+  
+// console.log(users[2])
+
+
+// let id ;
+let id;
+
+for(let i = 0 ; i < users[i].length ; i++){
+
+  
+  if(true){
+    console.log(users[i])
+  }
+ else{
+  console.log('no data')
+ }
+}
+
+// adding data using lenth 
+
+
+// users[users.length] = {id:4 , name:'ali akram'}
+// console.log(users)
+
+

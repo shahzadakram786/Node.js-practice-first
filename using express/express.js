22ee3cbc63658fe17c23b8ac9ab2425c3ff69115
shahzadakram786 <alyakram786@gmail.com>
2024-12-18 23:58:40 +0500

    al

--- using express/express.js
+++ using express/express.js
@@ -1,3 +1,16 @@
 const express = require('express')
 const app  = express()
 const Port = 2000;
+
+app.get('/',(req, res)=>{
+    res.send("hello world this is home page")
+})
+
+
+
+
+app.listen(Port,()=>{
+    console.log(`server is rinnig in https://localhost:${Port}`)
+})
+
+
